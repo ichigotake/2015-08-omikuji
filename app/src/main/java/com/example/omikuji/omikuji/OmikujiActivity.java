c4110ec37eccf1c2ea390e4fdab11ff5f83b7f55
ichigotake <ichigotake.san@gmail.com>
2015-07-31 00:46:56 +0900

    ボタンを押した時にランダムな結果を表示する
    JavaのRandomクラスを利用し、ランダムな数値を生成する
    2通りの結果を抽選するために、生成した数値を2で割った余りを結果として利用する
    
    アプリのリソース管理の基本
    http://mixi-inc.github.io/AndroidTraining/fundamentals/2.03.application-resource-management.html
    
    TextView#setTextに限らず、文字列を指定する際にR.stringを指定可能なメソッドも多数存在する

--- app/src/main/java/com/example/omikuji/omikuji/OmikujiActivity.java
+++ app/src/main/java/com/example/omikuji/omikuji/OmikujiActivity.java
@@ -3,9 +3,13 @@ package com.example.omikuji.omikuji;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class OmikujiActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView omikujiResultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,14 +17,29 @@ public class OmikujiActivity extends AppCompatActivity implements View.OnClickLi
         //findViewByIdをsetContentViewの前に実行するとnullが返るので注意する
         setContentView(R.layout.activity_omikuji);
         findViewById(R.id.omikuji).setOnClickListener(this);
+        omikujiResultView = (TextView) findViewById(R.id.omikuji_result);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.omikuji:
-                Toast.makeText(this, "クリックイベントの動作確認", Toast.LENGTH_SHORT).show();
+                omikuji();
                 break;
         }
     }
+
+    private void omikuji() {
+        Random random = new Random();
+        int result = random.nextInt() % 2;
+        switch (result) {
+            case 1:
+                omikujiResultView.setText(R.string.omikuji_result_daikichi);
+                break;
+            case 0:
+            default:
+                omikujiResultView.setText(R.string.omikuji_result_kichi);
+        }
+    }
+
 }
