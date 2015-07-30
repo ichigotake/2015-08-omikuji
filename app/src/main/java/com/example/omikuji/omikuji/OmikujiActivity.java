3e750180e097250062700cb6dce51e6a63a2acdc
ichigotake <ichigotake.san@gmail.com>
2015-07-31 00:39:01 +0900

    ボタンにクリックイベントを実装する
    クリックイベントの実装方法については以下を参考に。
    コメントも参考になる。
    よくわからなければ現段階では読まなくても問題なし。
    
    Android - 美しいOnClickListenerの実装方法(を教えてください) - Qiita
    http://qiita.com/t2low/items/8ac683c7ebf4b6dd1b41

--- app/src/main/java/com/example/omikuji/omikuji/OmikujiActivity.java
+++ app/src/main/java/com/example/omikuji/omikuji/OmikujiActivity.java
@@ -2,13 +2,25 @@ package com.example.omikuji.omikuji;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class OmikujiActivity extends AppCompatActivity {
+public class OmikujiActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //findViewByIdをsetContentViewの前に実行するとnullが返るので注意する
         setContentView(R.layout.activity_omikuji);
+        findViewById(R.id.omikuji).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.omikuji:
+                Toast.makeText(this, "クリックイベントの動作確認", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
